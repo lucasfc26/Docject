3ac6dc5c1c7a774f478d20a04d15cfd3fb31f495
lucasfc26 <lucasfcunha2626@gmail.com>
2026-06-22 11:55:54 -0300

    beta-test26.6.22.2

--- backend/src/modules/search/search.module.ts
+++ backend/src/modules/search/search.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "../../prisma/prisma.module";
 import { SearchController } from "./search.controller";
 import { SearchService } from "./search.service";
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SearchController],
   providers: [SearchService],
 })
